b5e774fd26751799c212aef6e9bc75944c708333
b3rdnikov06 <berdnikovpetr2019@gmail.com>
2026-08-17 20:00:33 +0300

    configure environment specific settings

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '17031703',
-    database: 'testing_system'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'testing_system'
 });
 
 connection.connect((err) => {
